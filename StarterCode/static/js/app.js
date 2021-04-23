392ef582b1562c323a81402b5b5fc58599485c83
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-04-22 23:59:58 -0500

    Have stubs for graphs

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,16 @@
 //This code was supplied by Dom during his office hours and 
 //therefore could be similar to other student's code
+function DrawBargraph(sampleid) {
+
+}
+
+function DrawBubbleChart(sampleid) {
+    
+}
+
+function ShowMetaData(sampleid) {
+    
+}
 function InitDashboard(){
     //Populate dropdown
     var selector = d3.select("#selDataset");
@@ -9,6 +20,10 @@ function InitDashboard(){
         samplenames.forEach(sampleID => {
             selector.append("option").text(sampleID).property("value", sampleID);
         });
+        var id = sampleNames[0];
+        DrawBargraph(id);
+        DrawBubbleChart(id);
+        ShowMetaData(id);
     });
     //Update Bargraph
 }
