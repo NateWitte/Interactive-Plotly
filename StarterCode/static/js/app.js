a7836356949938f02c18fb64c4e57ddaffce93a1
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-04-22 23:53:21 -0500

    Populated dropdown.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,8 +3,12 @@
 function InitDashboard(){
     //Populate dropdown
     var selector = d3.select("#selDataset");
-    d3.json("samples.json").then(function (data){
+    d3.json("samples.json").then(data => {
         console.log(data);
+        var samplenames = data.names;
+        samplenames.forEach(sampleID => {
+            selector.append("option").text(sampleID).property("value", sampleID);
+        });
     });
     //Update Bargraph
 }
